b3ad60f23461396815c3fedde6cd2e5c368fd4f4
Snowy97 <jhaiping999@gmail.com>
2020-09-13 20:02:49 +0800

    week_03

---
+++ Week_03/#leetcode_236二叉树的最近公共祖先.py
@@ -0,0 +1,24 @@
+#leetcode_236 二叉树的最近公共祖先
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        
+        if root is None:
+            return None
+        
+        if root is p or root is q:
+            return root
+        
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        
+        if left is not None and right is not None:
+            return root
+        
+        if left is not None:
+            return left
+        
+        if right is not None:
+            return right
+        
+        return None
